4d278cc028ab8e5adbc64ee0350302ec85a47e6d
Jugadh Singh Rally <84567746+Jugadhsing1324@users.noreply.github.com>
2026-05-12 09:50:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = "AIzaSyCsT4pn2wV8Iaw0YFRKgXWM3TyoT9OOi-8"; // <-- Replace this
+const GEMINI_API_KEY = "AlzaSyCsT4pn2wV8law0YFRKgXWM3TyoT9O0i-8"; // <-- Replace this
 
 const GEMINI_MODEL = "gemini-3.1-flash-lite";
 
